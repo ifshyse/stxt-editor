1b1cd4ce929aeba7d2b088074c1c70ffa8873c24
Stephen <sifangchenri@163.com>
2019-08-14 16:10:54 +0800

    font add
    font add

--- stxt-Editor/js/global.js
+++ stxt-Editor/js/global.js
@@ -1,3 +1,51 @@
+var fonts = []
+  fonts.push("PingFang SC");
+  fonts.push("Hiragino Sans GB");
+  fonts.push("microsoft yahei");
+  fonts.push("simsun");
+  fonts.push("cursive");
+  fonts.push("monospace");
+  fonts.push("serif");
+  fonts.push("sans-serif");
+  fonts.push("fantasy");
+  fonts.push("default");
+  fonts.push("Arial");
+  fonts.push("Arial Black");
+  fonts.push("Arial Rounded MT Bold");
+  fonts.push("Bookman Old Style");
+  fonts.push("Bradley Hand ITC");
+  fonts.push("Century");
+  fonts.push("Century Gothic");
+  fonts.push("Comic Sans MS");
+  fonts.push("Courier");
+  fonts.push("Courier New");
+  fonts.push("Georgia");
+  fonts.push("Gentium");
+  fonts.push("Impact");
+  fonts.push("King");
+  fonts.push("Lucida Console");
+  fonts.push("Lalit");
+  fonts.push("Modena");
+  fonts.push("Monotype Corsiva");
+  fonts.push("Papyrus");
+  fonts.push("Tahoma");
+  fonts.push("TeX");
+  fonts.push("Times");
+  fonts.push("Times New Roman");
+  fonts.push("Trebuchet MS");
+  fonts.push("Verdana");
+  fonts.push("Verona");
+
+var font_arr = []
+var d = new Detector();
+  for (i = 0; i < fonts.length; i++) {
+      var result = d.detect(fonts[i]);
+      if (result == true) {
+          font_arr.push(fonts[i]);
+      }
+  }
+  
+var f = ['SimSun', 'SimHei','Microsoft-YaHei','KaiTi','FangSong','Arial','Times-New-Roman','sans-serif'];  
 var toolbarOptions = [
   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
   ['blockquote', 'code-block'],
@@ -12,11 +60,15 @@ var toolbarOptions = [
   [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
   [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-  [{ 'font': [] }],
+  [{ 'font': f }],
   [{ 'align': [] }],
 
   ['clean']                                         // remove formatting button
 ];
+
+var Font = Quill.import('formats/font'); 
+Font.whitelist = f; //将字体加入到白名单 
+Quill.register(Font, true);
 
 var quill = new Quill('#editor-container', {
   modules: {
@@ -30,3 +82,12 @@ var quill = new Quill('#editor-container', {
 hljs.configure({   // optionally configure hljs
   languages: ['javascript', 'ruby', 'python']
 });
+
+// 提交表单
+function sub_post() {
+  var html = document.querySelector('#editor-container').children[0].innerHTML;
+  html = '<div class="ql-container ql-snow"><div class="ql-editor">'+html+"</div></div>";
+  $('[name=content]').val(html);
+
+  $("#submitForm").submit();
+}
